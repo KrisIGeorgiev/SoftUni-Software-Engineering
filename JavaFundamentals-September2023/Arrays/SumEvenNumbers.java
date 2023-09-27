c068af1769a92e76d9d9ca79093365271249c1b3
Kristian Georgiev <krisggeorgiev07@gmail.com>
2023-09-28 00:23:25 +0300

    Initial files added

---
+++ JavaFundamentals-September2023/Arrays/SumEvenNumbers.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class SumEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(e -> Integer.parseInt(e))
+                .toArray();
+
+        int sumEven = 0;
+
+        for (int index = 0; index < numbers.length; index++) {
+
+            if(numbers[index] % 2 == 0) {
+                sumEven += numbers[index];
+            }
+
+        }
+        System.out.println(sumEven);
+
+
+
+
+
+    }
+}
